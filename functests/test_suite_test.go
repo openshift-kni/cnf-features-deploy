5b231f24c4e3a58852652756bc5f60bd5ba4a3dc
David Vossel <davidvossel@gmail.com>
2020-01-09 13:19:16 -0500

    Add makefile and functional tests
    Signed-off-by: David Vossel <davidvossel@gmail.com>

---
+++ functests/test_suite_test.go
@@ -0,0 +1,55 @@
+// +build !unittests
+
+package test_test
+
+import (
+	"flag"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/reporters"
+	. "github.com/onsi/gomega"
+	testutils "github.com/openshift-kni/cnf-features-deploy/functests/utils"
+	testclient "github.com/openshift-kni/cnf-features-deploy/functests/utils/client"
+	"github.com/openshift-kni/cnf-features-deploy/functests/utils/namespaces"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// TODO: we should refactor tests to use client from controller-runtime package
+// see - https://github.com/openshift/cluster-api-actuator-pkg/blob/master/pkg/e2e/framework/framework.go
+
+var junitPath *string
+
+func init() {
+	junitPath = flag.String("junit", "junit.xml", "the path for the junit format report")
+}
+
+func TestTest(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	rr := []Reporter{}
+	if junitPath != nil {
+		rr = append(rr, reporters.NewJUnitReporter(*junitPath))
+	}
+	RunSpecsWithDefaultAndCustomReporters(t, "CNF Features e2e integratoin tests", rr)
+}
+
+var _ = BeforeSuite(func() {
+	// create test namespace
+	ns := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: testutils.NamespaceTesting,
+		},
+	}
+	_, err := testclient.Client.Namespaces().Create(ns)
+	Expect(err).ToNot(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	err := testclient.Client.Namespaces().Delete(testutils.NamespaceTesting, &metav1.DeleteOptions{})
+	Expect(err).ToNot(HaveOccurred())
+	err = namespaces.WaitForDeletion(testclient.Client, testutils.NamespaceTesting, 5*time.Minute)
+})
