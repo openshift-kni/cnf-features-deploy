1fae9feab38113f4d5e8d01b2656074a765e9526
David Vossel <davidvossel@gmail.com>
2020-01-09 13:41:05 -0500

    Add skeleton logic for feature deploy
    Signed-off-by: David Vossel <davidvossel@gmail.com>

---
+++ hack/feature-deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# expect oc to be in PATH by default
+OC_TOOL="${OC_TOOL:-oc}"
+
+for f in $FEATURES; do
+	echo "TODO add logic to implement feature '$f'"
+done
+
+echo "ERROR: FEATURE DEPLOY SCRIPT $0 NEEDS IMPLEMENTATION"
