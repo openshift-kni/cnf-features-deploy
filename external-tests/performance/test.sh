db4d0b4b4b89228a6a39c88541e1ef9d5c0d69f5
Federico Paolinelli <fpaoline@redhat.com>
2020-01-16 14:04:00 +0100

    Run external tests as part of the func test rule.
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- external-tests/performance/test.sh
+++ external-tests/performance/test.sh
@@ -1,8 +1,8 @@
-#/bin/bash
+#!/bin/bash
 
 export TESTS_REPO=https://github.com/openshift-kni/performance-addon-operators
 export TESTS_LOCATION=/tmp/performance-operators
 
 external-tests/clone_repo.sh
 cd $TESTS_LOCATION
-make functests
+make functests-only
