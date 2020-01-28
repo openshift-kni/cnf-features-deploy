b0049490ff630a53fbf3be370a55d08d798a94ab
Sebastian Sch <sebassch@gmail.com>
2020-01-28 19:01:46 +0200

    Add ptp labels on the nodes
    This PR add a label of grandmaster on the first master
    and all the other nodes (master/workers) are labeled as slaves
    
    Signed-off-by: Sebastian Sch <sebassch@gmail.com>

--- hack/setup-test-cluster.sh
+++ hack/setup-test-cluster.sh
@@ -13,6 +13,16 @@ do
     ${OC_TOOL} label $node node-role.kubernetes.io/worker-cnf=""
 done
 
+echo "[INFO]: Labeling first node as the ptp grandmaster"
+node=$(${OC_TOOL} get nodes -o name | sed -n 1p)
+${OC_TOOL} label $node ptp/grandmaster=""
+
+echo "[INFO]: Labeling all the other nodes as ptp slaves"
+nodes=$(${OC_TOOL} get nodes -o name | sed 1d)
+for node in $nodes
+do
+    ${OC_TOOL} label $node ptp/slave=""
+done
 
 # Note: this is intended to be the only pool we apply all mcs to.
 # Additional mcs must be added to this poll in the selector
