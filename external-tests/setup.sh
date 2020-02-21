2c558de8a81ed0c660043d7d60f4bac7448e797c
Federico Paolinelli <fpaoline@redhat.com>
2020-02-21 10:27:03 +0100

    Make the branch of externally consumed tests configurable. Also, make the version of the ose tests image configurable as well.
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

---
+++ external-tests/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Version of the ose tests image
+export OSE_TESTS_VERSION=latest
+
+# Branch of the performance addons operator to be used for tests
+export PERF_OPERATOR_BRANCH=master
