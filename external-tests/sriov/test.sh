708e182846c06ce4c58c4fc6b6932a2e3df20454
Sebastian Sch <sebassch@gmail.com>
2020-04-30 15:43:44 +0300

    Update sriov test to point sriov-tests.
    Base on: https://github.com/openshift/sriov-tests/pull/48
    
    Signed-off-by: Sebastian Sch <sebassch@gmail.com>

--- external-tests/sriov/test.sh
+++ external-tests/sriov/test.sh
@@ -2,10 +2,10 @@
 
 source "$(dirname "$0")/../setup.sh"
 
-export TESTS_REPO=https://github.com/openshift/sriov-network-operator
+export TESTS_REPO=https://github.com/openshift/sriov-tests
 export TESTS_LOCATION=/tmp/sriov-tests
 export REMOTE_BRANCH=$SRIOV_TESTS_BRANCH
 
 external-tests/clone_repo.sh
 cd $TESTS_LOCATION
-make test-e2e-conformance
+make conformance
