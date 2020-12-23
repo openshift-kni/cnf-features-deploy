1059e64b7b2e7601b5e4871a6a3b283bfe75628c
Jeff Uphoff <juphoff@redhat.com>
2020-12-23 07:43:55 -0500

    Remove RT-kernel check, reference env for mcp name
    - The RT-kernel check is a necessary but not sufficient condition for
      ensuring the node has been updated. Thus, it is being moved to the QE
      pipeline where it will be a part of other additional checks & tests.
    
    - Because "worker-cnf" is not in universal use ("worker-du" is being
      used in some cases), the mcp name can be passed in via an environment
      variable, with a current default of "worker-cnf" used if CNF_MCP is
      unset.

--- feature-configs/cn-ran-overlays/ran-profile/is_ready.sh
+++ feature-configs/cn-ran-overlays/ran-profile/is_ready.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-# Check for updated worker-cnf node
-
-oc wait mcp/worker-cnf --for condition=updated --timeout 1s
-
-
-# Check for real-time kernel.
-
-WORKER_NODE=$(oc get nodes --selector="node-role.kubernetes.io/worker-cnf" --output="name")
-
-echo "worker-cnf node is: ${WORKER_NODE}"
-
-oc describe "${WORKER_NODE}" | grep "Kernel Version:.*\.rt[0-9]*\..*\.x86_64"
+oc wait ${CNF_MCP:-"mcp/worker-cnf"} --for condition=updated --timeout 1s
