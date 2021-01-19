98e32dac4ea0934b92934a9d9479943ad6d450a2
Federico Paolinelli <fpaoline@redhat.com>
2021-01-19 19:07:36 +0100

    Makefile rule: deploy and validate only
    The reference layers (i.e. the ran overlays) require capabilities not
    available on cloud providers. However, deploying the manifests without
    errors and checking that the required operators are running has
    validation value. With this, we add a new validate-on-ci rule that runs
    only the validation suite.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- cnf-tests/test-run.sh
+++ cnf-tests/test-run.sh
@@ -3,9 +3,10 @@ set -e
 # Setting -e is fine as we want both config and validiation to succeed
 # before running the "real" tests.
 
-suites=(validationsuite configsuite cnftests)
+TEST_SUITES=${TEST_SUITES:-"validationsuite configsuite cnftests"}
 SUITES_PATH="${SUITES_PATH:-~/usr/bin}"
 
+suites=( $TEST_SUITES )
 if [ "$IMAGE_REGISTRY" != "" ] && [[ "$IMAGE_REGISTRY" != */ ]]; then
     export IMAGE_REGISTRY="$IMAGE_REGISTRY/"
 fi
