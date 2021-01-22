d14a503bd70b5aa901323e1f1510ff94401a9456
Federico Paolinelli <fpaoline@redhat.com>
2021-01-22 15:34:05 +0100

    Move the cnf-tests related files under cnftests
    The cnf-tests related content is scattered across the repo.
    Having all of them under the same root will make the repo more
    navigable.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

---
+++ cnf-tests/hack/common.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+pushd .
+cd "$(dirname "$0")/.."
+
+function finish {
+    popd
+}
+trap finish EXIT
+
+GOPATH="${GOPATH:-~/go}"
+export GOFLAGS="${GOFLAGS:-"-mod=vendor"}"
+
+export PATH=$PATH:$GOPATH/bin
