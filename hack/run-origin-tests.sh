a17782748011c264b12a1bf0b69b5958915e0a15
Sabina Aledort <saledort@redhat.com>
2021-01-31 16:00:17 +0200

    Allow running origin-tests in a disconnected environment
    Adding the option to specify the '--from-repository' flag which will
    source the required origin-tests images from the repository.

--- hack/run-origin-tests.sh
+++ hack/run-origin-tests.sh
@@ -11,30 +11,44 @@ trap finish_skopeo EXIT
 ORIGIN_TESTS_CONTAINER_MGMT_CLI="${ORIGIN_TESTS_CONTAINER_MGMT_CLI:-docker}"
 ORIGIN_TESTS_REPORTS_PATH="${ORIGIN_TESTS_REPORTS_PATH:-/tmp/artifacts/}"
 
+ORIGIN_TESTS_IN_DISCONNECTED_ENVIRONMENT="${ORIGIN_TESTS_IN_DISCONNECTED_ENVIRONMENT:-false}"
+
 ORIGIN_TESTS_IN_CONTAINER="${ORIGIN_TESTS_IN_CONTAINER:-true}"
 ORIGIN_TESTS_FILTER="${ORIGIN_TESTS_FILTER:-openshift/conformance/parallel}"
 CLUSTER_PROVIDER="${CLUSTER_PROVIDER:-}"
 
 failed=false
 
+if [ "$ORIGIN_TESTS_IN_DISCONNECTED_ENVIRONMENT" == "true" ]; then
+
+  # A container image repository to retrieve test images from.
+  # All the required test images should be mirrored to this repository.
+  if [ "$ORIGIN_TESTS_REPOSITORY" == "" ]; then
+    echo "[ERROR]: No ORIGIN_TESTS_REPOSITORY provided, can't run in disconnected mode"
+    exit 1
+  fi
+
+  flags="--from-repository $ORIGIN_TESTS_REPOSITORY"
+fi
+
 echo "Running $ORIGIN_TESTS_FILTER tests"
+
+# The cluster provider is the infrastructure provider (azure, aws, etc.).
+# Can be null when running on baremetal
+if [ -n "$CLUSTER_PROVIDER" ]; then
+  echo "Provider: $CLUSTER_PROVIDER"
+  flags="$flags --provider "${CLUSTER_PROVIDER}""
+fi
 
 mkdir -p "$ORIGIN_TESTS_REPORTS_PATH"
 
 mkdir -p _cache/
 cp -f "$KUBECONFIG" _cache/kubeconfig
 
-# The cluster provider is the infrastructure provider (azure, aws, etc.).
-# Can be null when running on baremetal
-if [ -n "$CLUSTER_PROVIDER" ]; then
-  echo "Provider: $CLUSTER_PROVIDER"
-  provider="--provider "${CLUSTER_PROVIDER}""
-fi
-
 if [ "$ORIGIN_TESTS_IN_CONTAINER" == "true" ]; then
   EXEC_TESTS="$ORIGIN_TESTS_CONTAINER_MGMT_CLI run -v $(pwd)/_cache/:/kubeconfig:Z -v $ORIGIN_TESTS_REPORTS_PATH:/reports:Z \
   -e KUBECONFIG=/kubeconfig/kubeconfig -it $ORIGIN_TESTS_IMAGE \
-  /usr/bin/openshift-tests run "$ORIGIN_TESTS_FILTER" ${provider} --junit-dir /reports"
+  /usr/bin/openshift-tests run "$ORIGIN_TESTS_FILTER" ${flags} --junit-dir /reports"
 else
   which skopeo
   if [ $? -ne 0 ]; then
@@ -49,7 +63,7 @@ else
   fi
 
   kubectl version
-  EXEC_TESTS="_cache/tools/openshift-tests run "$ORIGIN_TESTS_FILTER" ${provider} --junit-dir $ORIGIN_TESTS_REPORTS_PATH"
+  EXEC_TESTS="_cache/tools/openshift-tests run "$ORIGIN_TESTS_FILTER" ${flags} --junit-dir $ORIGIN_TESTS_REPORTS_PATH"
 fi
 
 if ! $EXEC_TESTS; then
