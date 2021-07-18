89284ec6a3c1f3365c21c361da8ad17a5603fde0
Yuval Kashtan <yuvalkashtan@gmail.com>
2021-07-18 23:02:51 +0300

    cnf-tests: WIP move to sync
    a. apparently there's an easy built-in package, no need to invent
    semaphore
    b. ginkgo doesnt support parallelism, so instead I'm doing it inside a
    single test

--- cnf-tests/testsuites/flakesuite/empty_string/flakes.go
+++ cnf-tests/testsuites/flakesuite/empty_string/flakes.go
@@ -2,7 +2,9 @@ package empty_string
 
 import (
 	"context"
+	"fmt"
 	"strconv"
+	"sync"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -19,44 +21,6 @@ import (
 	"github.com/openshift-kni/cnf-features-deploy/cnf-tests/testsuites/pkg/namespaces"
 	"github.com/openshift-kni/cnf-features-deploy/cnf-tests/testsuites/pkg/pods"
 )
-
-type empty struct{}
-type semaphore chan empty
-
-// acquire n resources
-func (s semaphore) P(n int) {
-	e := empty{}
-	for i := 0; i < n; i++ {
-		s <- e
-	}
-}
-
-// release n resources
-func (s semaphore) V(n int) {
-	for i := 0; i < n; i++ {
-		<-s
-	}
-}
-
-/* mutexes */
-
-func (s semaphore) Lock() {
-	s.P(1)
-}
-
-func (s semaphore) Unlock() {
-	s.V(1)
-}
-
-/* signal-wait */
-
-func (s semaphore) Signal() {
-	s.V(1)
-}
-
-func (s semaphore) Wait(n int) {
-	s.P(n)
-}
 
 var (
 	machineConfigPoolNodeSelector string
@@ -83,16 +47,18 @@ var _ = Describe("flaketests", func() {
 			}
 		})
 
-		sem := make(semaphore, 100)
-		for i := 0; i < 100; i++ {
-			go func(i int) {
-				It("should return the echo string", func() {
-					By("Create a pod")
-					pod := simplePod("empty_strings-" + strconv.Itoa(i))
+		It("should return the echo string", func() {
+			var wg sync.WaitGroup
+			var results sync.Map
+
+			for i := 0; i < 100; i++ {
+				wg.Add(1)
+				go func(i int) {
+					podName := "empty_strings-" + strconv.Itoa(i)
+					pod := simplePod(podName)
 					simplePod, err := client.Client.Pods(namespaces.FlakesTest).Create(context.Background(), pod, metav1.CreateOptions{})
 					Expect(err).ToNot(HaveOccurred())
 
-					By("Check if the pod is running")
 					Eventually(func() corev1.PodPhase {
 						runningPod, err := client.Client.Pods(namespaces.FlakesTest).Get(context.Background(), simplePod.Name, metav1.GetOptions{})
 
@@ -100,16 +66,18 @@ var _ = Describe("flaketests", func() {
 						return runningPod.Status.Phase
 					}, 1*time.Minute, 1*time.Second).Should(Equal(corev1.PodRunning))
 
-					By("comparing Echo")
 					cmd := []string{"echo", "test"}
 					out, err := pods.ExecCommand(client.Client, *simplePod, cmd)
 					Expect(out.String()).Should(ContainSubstring("test"))
-				})
-				sem.Signal()
-			}(i)
-		}
-		sem.Wait(100)
-
+					results.Store(podName, out)
+				}(i)
+			}
+			wg.Wait()
+			results.Range(func(key, value interface{}) bool {
+				fmt.Printf("test %s %s\n", key, value)
+				return true
+			})
+		})
 	})
 })
 
