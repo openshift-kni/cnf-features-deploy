a91259610f5c1d80ca7fb2cc4b52d4e47b3ce344
Yuval Kashtan <yuvalkashtan@gmail.com>
2021-07-18 13:18:12 +0300

    cnf-tests: WIP add a simple pod test

--- cnf-tests/testsuites/flakesuite/empty_string/flakes.go
+++ cnf-tests/testsuites/flakesuite/empty_string/flakes.go
@@ -2,29 +2,21 @@ package empty_string
 
 import (
 	"context"
-	//	"encoding/json"
-	//	"fmt"
-	//	"log"
-	//	"os"
-	//	"strings"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	//	corev1 "k8s.io/api/core/v1"
-	//	apiext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	//	"k8s.io/apimachinery/pkg/api/errors"
-	//	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	//	goclient "sigs.k8s.io/controller-runtime/pkg/client"
-
-	//	sriovv1 "github.com/k8snetworkplumbingwg/sriov-network-operator/api/v1"
 	clientmachineconfigv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/openshift-kni/cnf-features-deploy/cnf-tests/testsuites/pkg/client"
 	testclient "github.com/openshift-kni/cnf-features-deploy/cnf-tests/testsuites/pkg/client"
-	//	"github.com/openshift-kni/cnf-features-deploy/cnf-tests/testsuites/pkg/namespaces"
-	//	utilNodes "github.com/openshift-kni/cnf-features-deploy/cnf-tests/testsuites/pkg/nodes"
-	//	"github.com/openshift-kni/cnf-features-deploy/cnf-tests/testsuites/pkg/sriov"
-	//	"github.com/openshift-kni/cnf-features-deploy/cnf-tests/testsuites/pkg/utils"
+	"github.com/openshift-kni/cnf-features-deploy/cnf-tests/testsuites/pkg/execute"
+	"github.com/openshift-kni/cnf-features-deploy/cnf-tests/testsuites/pkg/images"
+	"github.com/openshift-kni/cnf-features-deploy/cnf-tests/testsuites/pkg/namespaces"
+	"github.com/openshift-kni/cnf-features-deploy/cnf-tests/testsuites/pkg/pods"
 )
 
 var (
@@ -34,7 +26,13 @@ var (
 func init() {
 }
 
-var _ = Describe("validation", func() {
+var _ = Describe("flaketests", func() {
+	execute.BeforeAll(func() {
+		var err error
+		err = namespaces.Create(namespaces.FlakesTest, client.Client)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
 	Context("general", func() {
 		It("should report all machine config pools are in ready status", func() {
 			mcp := &clientmachineconfigv1.MachineConfigPoolList{}
@@ -46,5 +44,50 @@ var _ = Describe("validation", func() {
 			}
 		})
 
+		It("should return the echo string", func() {
+			By("Create a pod")
+			pod := simplePod("empty_strings")
+			simplePod, err := client.Client.Pods(namespaces.FlakesTest).Create(context.Background(), pod, metav1.CreateOptions{})
+			Expect(err).ToNot(HaveOccurred())
+
+			By("Check if the pod is running")
+			Eventually(func() corev1.PodPhase {
+				runningPod, err := client.Client.Pods(namespaces.FlakesTest).Get(context.Background(), simplePod.Name, metav1.GetOptions{})
+
+				Expect(err).ToNot(HaveOccurred())
+				return runningPod.Status.Phase
+			}, 1*time.Minute, 1*time.Second).Should(Equal(corev1.PodRunning))
+
+			By("comparing Echo")
+			cmd := []string{"echo", "test"}
+			out, err := pods.ExecCommand(client.Client, *simplePod, cmd)
+			Expect(out.String()).Should(ContainSubstring("test"))
+		})
+
 	})
 })
+
+func simplePod(name string) *corev1.Pod {
+	res := corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+			Labels: map[string]string{
+				"name": name,
+			},
+			Namespace: namespaces.FlakesTest,
+		},
+		Spec: corev1.PodSpec{
+			RestartPolicy: corev1.RestartPolicyNever,
+			Containers: []corev1.Container{
+				{
+					Name:    name,
+					Image:   images.For(images.TestUtils),
+					Command: []string{"/bin/sh", "-c"},
+					Args:    []string{"sleep inf"},
+				},
+			},
+		},
+	}
+
+	return &res
+}
