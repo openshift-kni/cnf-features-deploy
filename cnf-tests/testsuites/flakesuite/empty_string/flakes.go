67567925ffdc27823325fdd14e1b9bef6d06971b
Yuval Kashtan <yuvalkashtan@gmail.com>
2021-07-18 22:30:24 +0300

    cnf-tests: WIP - parallel with semaphore
    100 times for now

--- cnf-tests/testsuites/flakesuite/empty_string/flakes.go
+++ cnf-tests/testsuites/flakesuite/empty_string/flakes.go
@@ -2,6 +2,7 @@ package empty_string
 
 import (
 	"context"
+	"strconv"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -18,6 +19,44 @@ import (
 	"github.com/openshift-kni/cnf-features-deploy/cnf-tests/testsuites/pkg/namespaces"
 	"github.com/openshift-kni/cnf-features-deploy/cnf-tests/testsuites/pkg/pods"
 )
+
+type empty struct{}
+type semaphore chan empty
+
+// acquire n resources
+func (s semaphore) P(n int) {
+	e := empty{}
+	for i := 0; i < n; i++ {
+		s <- e
+	}
+}
+
+// release n resources
+func (s semaphore) V(n int) {
+	for i := 0; i < n; i++ {
+		<-s
+	}
+}
+
+/* mutexes */
+
+func (s semaphore) Lock() {
+	s.P(1)
+}
+
+func (s semaphore) Unlock() {
+	s.V(1)
+}
+
+/* signal-wait */
+
+func (s semaphore) Signal() {
+	s.V(1)
+}
+
+func (s semaphore) Wait(n int) {
+	s.P(n)
+}
 
 var (
 	machineConfigPoolNodeSelector string
@@ -44,27 +83,32 @@ var _ = Describe("flaketests", func() {
 			}
 		})
 
-		for i := 0; i < 1000; i++ {
-			It("should return the echo string", func() {
-				By("Create a pod")
-				pod := simplePod("empty_strings")
-				simplePod, err := client.Client.Pods(namespaces.FlakesTest).Create(context.Background(), pod, metav1.CreateOptions{})
-				Expect(err).ToNot(HaveOccurred())
-
-				By("Check if the pod is running")
-				Eventually(func() corev1.PodPhase {
-					runningPod, err := client.Client.Pods(namespaces.FlakesTest).Get(context.Background(), simplePod.Name, metav1.GetOptions{})
-
+		sem := make(semaphore, 100)
+		for i := 0; i < 100; i++ {
+			go func(i int) {
+				It("should return the echo string", func() {
+					By("Create a pod")
+					pod := simplePod("empty_strings-" + strconv.Itoa(i))
+					simplePod, err := client.Client.Pods(namespaces.FlakesTest).Create(context.Background(), pod, metav1.CreateOptions{})
 					Expect(err).ToNot(HaveOccurred())
-					return runningPod.Status.Phase
-				}, 1*time.Minute, 1*time.Second).Should(Equal(corev1.PodRunning))
 
-				By("comparing Echo")
-				cmd := []string{"echo", "test"}
-				out, err := pods.ExecCommand(client.Client, *simplePod, cmd)
-				Expect(out.String()).Should(ContainSubstring("test"))
-			})
+					By("Check if the pod is running")
+					Eventually(func() corev1.PodPhase {
+						runningPod, err := client.Client.Pods(namespaces.FlakesTest).Get(context.Background(), simplePod.Name, metav1.GetOptions{})
+
+						Expect(err).ToNot(HaveOccurred())
+						return runningPod.Status.Phase
+					}, 1*time.Minute, 1*time.Second).Should(Equal(corev1.PodRunning))
+
+					By("comparing Echo")
+					cmd := []string{"echo", "test"}
+					out, err := pods.ExecCommand(client.Client, *simplePod, cmd)
+					Expect(out.String()).Should(ContainSubstring("test"))
+				})
+				sem.Signal()
+			}(i)
 		}
+		sem.Wait(100)
 
 	})
 })
