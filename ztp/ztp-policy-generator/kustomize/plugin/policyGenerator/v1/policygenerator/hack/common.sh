9e64c18184e68a545f043029fcc5193d38d926aa
Jim Ramsay <jramsay@redhat.com>
2021-08-01 21:53:22 -0400

    ztp: add go vet hack enforcement script
    Signed-off-by: Jim Ramsay <jramsay@redhat.com>

---
+++ ztp/ztp-policy-generator/kustomize/plugin/policyGenerator/v1/policygenerator/hack/common.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+pushd "$(dirname "$0")/.."
+
+function finish {
+    popd
+}
+trap finish EXIT
+
+GOPATH="${GOPATH:-~/go}"
+export GOFLAGS="${GOFLAGS:-"-mod=vendor"}"
+
+export PATH=$PATH:$GOPATH/bin
+
+mkdir -p _cache
