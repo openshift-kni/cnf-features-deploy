f67175f79a7b6991ac02a6ee87cb8f3aca2c217a
Talor Itzhak <titzhak@redhat.com>
2021-08-05 14:02:15 +0300

    cnf-tests: Control CPUs amount under latency-test pod
    By having the new LATENCY_TEST_CPUS env variable, the user will be able to specify the amount of CPUs
    that will be used by the pod which runs the latency tests.
    
    The LATENCY_TEST_CPUS env variable is optional, the default value is the total amount of isolated CPUs minus one (this behavior is identical to the prior one).
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- cnf-tests/pod-utils/cyclictest-runner/main.go
+++ cnf-tests/pod-utils/cyclictest-runner/main.go
@@ -17,7 +17,6 @@ func main() {
 
 	rtPriority := flag.String("rt-priority", "1", "specify the SCHED_FIFO priority (1-99)")
 	duration := flag.String("duration", "15s", "specify a length for the test run. Append 'm', 'h', or 'd' to specify minutes, hours or days.")
-	threads := flag.Int("threads", 1, "number of threads: if -1, number of threads will be max_cpus. without -t default = 1")
 	histogram := flag.String("histogram", "30", "dump a latency histogram to stdout after the run US is the max latency time to be be tracked in microseconds")
 	interval := flag.Int("interval", 1000, "base interval of thread in us default=1000")
 	cyclictestStartDelay := flag.Duration("cyclictest-start-delay", 0, "delay in second before running the cyclictest binary")
@@ -27,10 +26,6 @@ func main() {
 	selfCPUs, err := node.GetSelfCPUs()
 	if err != nil {
 		klog.Fatalf("failed to get self allowed CPUs: %v", err)
-	}
-
-	if *threads == -1 {
-		*threads = selfCPUs.Size()
 	}
 
 	err = node.PrintInformation()
@@ -43,7 +38,7 @@ func main() {
 	cyclictestArgs := []string{
 		"-D", *duration,
 		"-p", *rtPriority,
-		"-t", strconv.Itoa(*threads),
+		"-t", strconv.Itoa(selfCPUs.Size()),
 		"-a", selfCPUs.String(),
 		"-h", *histogram,
 		"-i", strconv.Itoa(*interval),
