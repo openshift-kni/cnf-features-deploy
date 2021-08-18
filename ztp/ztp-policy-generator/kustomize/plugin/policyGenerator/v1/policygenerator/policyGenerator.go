5993f3f292f566c40a65ee1daf1ee8e7d79ced01
Ian Miller <imiller@redhat.com>
2021-08-18 15:14:02 -0400

    ztp: fixes for argoCD flow
        - When errors are detected in policy or site generation the program
          returned a 0 status code indicating normal completion. This caused
          errors in generating some CRs to be hidden from the user in the ArgoCD
          pipeline flow. Adding in a panic() after any generated policies are
          written out causes the pipeline job to fail and the logs will contain
          critical debug information.
    
    Signed-off-by: Ian Miller <imiller@redhat.com>
    
    ztp: argocd CRs and container Makefile updates
    
        - Fix deploy path
        - allow patching policies
        - Fix patching resources
        - revert prunelast
    
    ztp: Enable ArgoCD hook to create ACM policy association
    
        - The ArgoCD hooks run in the site-subs namespace using the
        system:serviceaccount:sites-sub:default ServiceAccount. This account
        must be have sufficient role privilege to create placementrules and
        placementbindings for ACM.
    
    Signed-off-by: Ian Miller <imiller@redhat.com>

--- ztp/ztp-policy-generator/kustomize/plugin/policyGenerator/v1/policygenerator/policyGenerator.go
+++ ztp/ztp-policy-generator/kustomize/plugin/policyGenerator/v1/policygenerator/policyGenerator.go
@@ -57,6 +57,7 @@ func InitiatePolicyGen(tempPath string, sourcePath string, outPath string, stdou
 
 			if err != nil {
 				fmt.Println(err)
+				// The error will be raised after we write out whatever policy we can
 			}
 			for k, v := range policies {
 				policy, _ := yaml.Marshal(v)
@@ -66,22 +67,28 @@ func InitiatePolicyGen(tempPath string, sourcePath string, outPath string, stdou
 				}
 				fHandler.WriteFile(k+utils.FileExt, policy)
 			}
-		} else if kindType.Kind == "SiteConfig" {
+			if err != nil {
+				panic(err)
+			}
+ 		} else if kindType.Kind == "SiteConfig" {
 			siteConfig := siteConfigs.SiteConfig{}
 			err := yaml.Unmarshal(yamlFile, &siteConfig)
 			if err != nil {
 				fmt.Println(err)
+				panic(err)
 			}
 			var buffer bytes.Buffer
 			scBuilder, err := siteConfigs.NewSiteConfigBuilder(fHandler)
 
 			if err != nil {
 				fmt.Println(err)
+				panic(err)
 			}
 			clusters, err := scBuilder.Build(siteConfig)
 
 			if err != nil {
 				fmt.Println(err)
+				// Error will be raised below after writing out whatever CR we can
 			}
 			for clusterName, crs := range clusters {
 				for _, crIntf := range crs {
@@ -92,12 +99,15 @@ func InitiatePolicyGen(tempPath string, sourcePath string, outPath string, stdou
 					buffer.Write(siteConfigs.Separator)
 					buffer.Write(cr)
 				}
-	
+
 				if stdout {
 					fmt.Println(buffer.String())
 				}
 				fHandler.WriteFile(clusterName+utils.FileExt, buffer.Bytes())
 				buffer.Reset()
+			}
+			if err != nil {
+				panic(err)
 			}
 		} else {
 			log.Println("Not_supported_template ", kindType)
