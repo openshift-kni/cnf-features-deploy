3fd1e4a28dd28cf7fbbefb154434e81e8f9fb3f4
Vitaly Grinberg <vgrinber@redhat.com>
2021-09-05 11:52:10 +0300

    ztp: Bug 2000726: fix rv data name and path
    The post-sync resource hook was failing due to an expired resource version.
    The reason: mistakenly initialized the resource RV from the config map RV
    instead of its data.

--- ztp/resource-generator/src/get-pre-sync-rv.sh
+++ ztp/resource-generator/src/get-pre-sync-rv.sh
@@ -2,7 +2,7 @@
 
 CM=$(oc get configmap/rv &> /dev/null)
 if (( $? == 0 )); then
-  RV=$( oc get configmap/rv -ojson | jq -rM '.metadata.resourceVersion' )
+  RV=$( oc get configmap/rv -ojson | jq -rM '.data.sitesResourceVersion' )
   echo "ztp-hooks.postsync $(date -R) INFO [post-sync-entrypoint] Retrieved RAN sites resourceVersion $RV" >> /proc/1/fd/2
   echo $RV
 else
