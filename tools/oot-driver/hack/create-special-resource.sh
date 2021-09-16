795265c99d20b598772c2a0154019a9399d9e393
Eric <eric@example.com>
2021-09-16 13:10:34 +0000

    oot-driver: New makefile var to allow configuring KERNEL_SOURCE + fix for specialresource driver container names

--- tools/oot-driver/hack/create-special-resource.sh
+++ tools/oot-driver/hack/create-special-resource.sh
@@ -3,10 +3,10 @@ set -eu
 
 export DRIVER_TOOLKIT_IMAGE=$1; shift
 export EXTERNAL_REGISTRY=$1; shift
-export OOT_DRIVER_IMAGE_NAME=$1; shift
 export SIGN_DRIVER=$1; shift
 export DOWNLOAD_DRIVER=$1; shift
 export KERNEL_VERSION_LIST=$1; shift
+export KERNEL_SOURCE=$1; shift
 
 rm -f "./special-resource.yaml" || true
 IFS=',' read -r -a array <<< "${KERNEL_VERSION_LIST}"
@@ -16,6 +16,7 @@ for f in $FILES
 do
   for index in "${!array[@]}"
   do
+      export OOT_DRIVER_IMAGE_NAME=$(basename ${f/special-resource.yaml.template/container/})
       export KERNEL_VERSION=${array[index]}
       export INDEX=${index}
       envsubst < "$f" >> "./special-resource.yaml"
