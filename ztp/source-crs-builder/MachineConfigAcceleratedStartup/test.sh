fcb7e881d97cd1a18efeaf045dab939ca1d1d1b1
Jim Ramsay <jramsay@redhat.com>
2021-09-21 11:15:34 -0400

    ztp: Add more robust logic to accelerated-startup CPUset detection
    Also fixes up some comments in the services files to make customization
    more clear.
    
    Signed-off-by: Jim Ramsay <jramsay@redhat.com>

--- ztp/source-crs-builder/MachineConfigAcceleratedStartup/test.sh
+++ ztp/source-crs-builder/MachineConfigAcceleratedStartup/test.sh
@@ -15,15 +15,7 @@ test_within() {
   local expected=$1; shift
   within "$@"
   rc=$?
-  [[ $rc -eq $expected ]] || fatal "within failed: Expected rc $rc != $expected"
-}
-
-test_steadystate() {
-  local expected=$1
-  STEADY_STATE_THRESHOLD=$4
-  steadystate $2 $3
-  rc=$?
-  [[ $rc -eq $expected ]] || fatal "steadystate failed: Expected rc $rc != $expected"
+  [[ $rc -eq $expected ]] || fatal "within failed: Expected rc $expected != $rc"
 }
 
 # Trivial case: 0->0 = 0% change
@@ -98,16 +90,52 @@ test_within 0 102 100 1%
 test_within 0 102 100 2
 test_within 0 102 100 2%
 
-# Test with/without allowed-zero
-echo "Allowing last >= 0"
+test_steadystate() {
+  local expected=$1
+  STEADY_STATE_THRESHOLD=$4
+  steadystate $2 $3
+  rc=$?
+  [[ $rc -eq $expected ]] || fatal "steadystate failed: Expected rc $expected != $rc"
+}
+
+# Test with minimum boundary
+echo "Allowing minimum >= 0"
 STEADY_STATE_MINIMUM=0
 test_steadystate 0 0 0 0
 test_steadystate 0 1 0 1
 test_steadystate 0 2 3 2
-echo "Only allowing last >= 2"
+echo "Only allowing minimum >= 2"
 STEADY_STATE_MINIMUM=2
 test_steadystate 1 0 0 0
 test_steadystate 1 1 0 1
 test_steadystate 0 2 3 2
+
+test_unrestrictedCpuset() {
+    local expectedRc="$1" expectedValue="$2" kubeletContent="$3" defaultState="$4"
+    echo "Testing unrestrictedCpuset: $5"
+    KUBELET_CPU_STATE=/tmp/$$-kubelet-state
+    FULL_CPU_STATE=/tmp/$$-default-state
+    if [[ -n $kubeletContent ]]; then
+        touch $KUBELET_CPU_STATE
+        if [[ $kubeletContent != ' ' ]]; then
+            echo "$kubeletContent" > $KUBELET_CPU_STATE
+        fi
+    fi
+    if [[ -n $defaultState ]]; then
+        echo "$defaultState" > $FULL_CPU_STATE
+    fi
+    local result
+    result=$(unrestrictedCpuset)
+    local rc=$?
+    rm -f $KUBELET_CPU_STATE $FULL_CPU_STATE
+    [[ $rc -eq $expectedRc ]] || fatal "  unrestrictedCpuset failed: Expected rc $expectedRc != $rc"
+    [[ $result == $expectedValue ]] || fatal "  unrestrictedCpuset failed: Expected return value '$expectedValue' != '$result'"
+}
+
+test_unrestrictedCpuset 0 "4-5" '{ "defaultCpuSet": "4-5" }' "0-9" "Valid parse results"
+test_unrestrictedCpuset 0 "0-9" '{ INVALID JSON t": "4-5" }' "0-9" "Invalid json fallback"
+test_unrestrictedCpuset 0 "0-9" ' '                          "0-9" "Empty file fallback"
+test_unrestrictedCpuset 0 "0-9" ''                           "0-9" "Missing file falback"
+test_unrestrictedCpuset 1 ''    ''                           ''    "Fallback file missing"
 
 echo "All tests completed successfully!"
