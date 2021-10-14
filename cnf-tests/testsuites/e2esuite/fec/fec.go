d6f1798f2e562062e856f6ec6d3faea22a617487
Sebastian Sch <sebassch@gmail.com>
2021-10-14 12:46:33 +0300

    cnf-tests: update fec tests to use v2 api for intel fec operator
    Signed-off-by: Sebastian Sch <sebassch@gmail.com>

--- cnf-tests/testsuites/e2esuite/fec/fec.go
+++ cnf-tests/testsuites/e2esuite/fec/fec.go
@@ -14,7 +14,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtimeClient "sigs.k8s.io/controller-runtime/pkg/client"
 
-	sriovfecv1 "github.com/open-ness/openshift-operator/sriov-fec/api/v1"
+	sriovfecv2 "github.com/smart-edge-open/openshift-operator/sriov-fec/api/v2"
 
 	"github.com/openshift-kni/cnf-features-deploy/cnf-tests/testsuites/pkg/client"
 	"github.com/openshift-kni/cnf-features-deploy/cnf-tests/testsuites/pkg/discovery"
@@ -54,11 +54,6 @@ var _ = Describe("fec", func() {
 		Expect(err).ToNot(HaveOccurred())
 	})
 
-	AfterEach(func() {
-		err := restoreDefaultConfig()
-		Expect(err).ToNot(HaveOccurred())
-	})
-
 	Context("Expose resource on the node", func() {
 		It("should show resources under the node", func() {
 			Eventually(func() int64 {
@@ -71,31 +66,6 @@ var _ = Describe("fec", func() {
 		})
 	})
 })
-
-func restoreDefaultConfig() error {
-	sriovFecCluster := &sriovfecv1.SriovFecClusterConfig{}
-	err := client.Client.Get(context.TODO(), runtimeClient.ObjectKey{Name: sriovFecClusterConfigName, Namespace: namespaces.IntelOperator}, sriovFecCluster)
-	if err != nil {
-		return nil
-	}
-
-	sriovFecCluster.Spec.Nodes[0].PhysicalFunctions[0].VFAmount = 0
-
-	err = client.Client.Update(context.TODO(), sriovFecCluster)
-	if err != nil {
-		return nil
-	}
-
-	Eventually(func() int64 {
-		testedNode, err := client.Client.Nodes().Get(context.Background(), sriovFecCluster.Spec.Nodes[0].NodeName, metav1.GetOptions{})
-		Expect(err).ToNot(HaveOccurred())
-		resNum, _ := testedNode.Status.Allocatable[corev1.ResourceName(acc100ResourceName)]
-		allocatable, _ := resNum.AsInt64()
-		return allocatable
-	}, 10*time.Minute, time.Second).Should(Equal(int64(0)))
-
-	return nil
-}
 
 func getAcc100Device() (string, string, error) {
 	nodesWithAcc100, err := getSriovFecNodes()
@@ -117,36 +87,35 @@ func getAcc100Device() (string, string, error) {
 }
 
 func createSriovFecClusterObject(nodeName string, pciAddress string, numVfs int, isSingleNode bool) error {
-	queueGroupConfig := sriovfecv1.QueueGroupConfig{
+	queueGroupConfig := sriovfecv2.QueueGroupConfig{
 		AqDepthLog2:     4,
 		NumAqsPerGroups: 16,
 		NumQueueGroups:  2,
 	}
 
-	sriovFecClusterConfig := &sriovfecv1.SriovFecClusterConfig{
+	sriovFecClusterConfig := &sriovfecv2.SriovFecClusterConfig{
 		ObjectMeta: metav1.ObjectMeta{Name: sriovFecClusterConfigName, Namespace: namespaces.IntelOperator},
-		Spec: sriovfecv1.SriovFecClusterConfigSpec{
-			Nodes: []sriovfecv1.NodeConfig{
-				{
-					NodeName: nodeName,
-					PhysicalFunctions: []sriovfecv1.PhysicalFunctionConfig{
-						{
-							PCIAddress: pciAddress,
-							PFDriver:   "pci-pf-stub",
-							VFAmount:   numVfs,
-							VFDriver:   "vfio-pci",
-							BBDevConfig: sriovfecv1.BBDevConfig{
-								ACC100: &sriovfecv1.ACC100BBDevConfig{
-									Downlink4G:   queueGroupConfig,
-									Downlink5G:   queueGroupConfig,
-									Uplink4G:     queueGroupConfig,
-									Uplink5G:     queueGroupConfig,
-									PFMode:       false,
-									MaxQueueSize: 1024,
-									NumVfBundles: 16,
-								},
-							},
-						},
+		Spec: sriovfecv2.SriovFecClusterConfigSpec{
+			Priority: 1,
+			NodeSelector: map[string]string{
+				"kubernetes.io/hostname": nodeName,
+			},
+			AcceleratorSelector: sriovfecv2.AcceleratorSelector{
+				PCIAddress: pciAddress,
+			},
+			PhysicalFunction: sriovfecv2.PhysicalFunctionConfig{
+				PFDriver: "pci-pf-stub",
+				VFAmount: numVfs,
+				VFDriver: "vfio-pci",
+				BBDevConfig: sriovfecv2.BBDevConfig{
+					ACC100: &sriovfecv2.ACC100BBDevConfig{
+						Downlink4G:   queueGroupConfig,
+						Downlink5G:   queueGroupConfig,
+						Uplink4G:     queueGroupConfig,
+						Uplink5G:     queueGroupConfig,
+						PFMode:       false,
+						MaxQueueSize: 1024,
+						NumVfBundles: numVfs,
 					},
 				},
 			},
@@ -164,7 +133,7 @@ func createSriovFecClusterObject(nodeName string, pciAddress string, numVfs int,
 func getSriovFecNodes() ([]string, error) {
 	nodesWithAcc100 := []string{}
 
-	sriovFecNodeList := &sriovfecv1.SriovFecNodeConfigList{}
+	sriovFecNodeList := &sriovfecv2.SriovFecNodeConfigList{}
 	err := client.Client.List(context.TODO(), sriovFecNodeList, &runtimeClient.ListOptions{Namespace: namespaces.IntelOperator})
 	if err != nil {
 		return nil, err
@@ -182,7 +151,7 @@ func getSriovFecNodes() ([]string, error) {
 }
 
 func getAcc100PciFromNode(nodeName string) (string, error) {
-	sriovFecNodeConfig := &sriovfecv1.SriovFecNodeConfig{}
+	sriovFecNodeConfig := &sriovfecv2.SriovFecNodeConfig{}
 	err := client.Client.Get(context.TODO(), runtimeClient.ObjectKey{Name: nodeName, Namespace: namespaces.IntelOperator}, sriovFecNodeConfig)
 	if err != nil {
 		return "", err
@@ -198,7 +167,7 @@ func getAcc100PciFromNode(nodeName string) (string, error) {
 }
 
 func Clean() {
-	sriovFecCluster := &sriovfecv1.SriovFecClusterConfig{}
+	sriovFecCluster := &sriovfecv2.SriovFecClusterConfig{}
 	err := client.Client.Get(context.TODO(), runtimeClient.ObjectKey{Name: sriovFecClusterConfigName, Namespace: namespaces.IntelOperator}, sriovFecCluster)
 	if meta.IsNoMatchError(err) || errors.IsNotFound(err) {
 		return
