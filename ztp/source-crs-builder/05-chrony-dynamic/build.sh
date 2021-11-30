d2d5e34e96035e29c98ca3844ab78da1fc4b64fd
Jim Ramsay <jramsay@redhat.com>
2021-11-30 16:31:58 -0500

    ztp: Generate all static MachineConfig objects for both workers and masters
    For 4.10 we're fine with just duplicating all static MachineConfig
    objects for both "master" and "worker" roles explicitly for all
    installs:
    - For SNO, it's safe to have additional worker profiles sitting there
      dormant
    - Even if additional worker-related pools are defined, they're always
      set up to inherit from the 'worker' mcp and there aren't any MC
      objects we have set up that are unsafe to just globally apply to all
      masters and all workers and all potential worker variations.
    
    Additionally, all of these MachineConfig files are automaticaly
    genereted by the ztp/extra-manifests-builder infrastructure and kept
    in-sync by the 'make ci-job' check so there's no danger of any
    copy&paste errors or master and worker files drifting apart in the
    future.
    
    Signed-off-by: Jim Ramsay <jramsay@redhat.com>

--- ztp/source-crs-builder/05-chrony-dynamic/build.sh
+++ ztp/source-crs-builder/05-chrony-dynamic/build.sh
@@ -3,11 +3,11 @@
 GOPATH=${GOPATH:-${HOME}/go}
 GOBIN=${GOBIN:-${GOPATH}/bin}
 MCMAKER=${MCMAKER:-${GOBIN}/mcmaker}
+MCPROLE=${MCPROLE:-master}
 
-${MCMAKER} -stdout -name 05-chronyd-dynamic -mcp master \
+${MCMAKER} -stdout -name 05-chronyd-dynamic -mcp ${MCPROLE} \
 	file -source ptp-sync-check -path /usr/local/bin/ptp-sync-check -mode 0755 \
 	file -source restart-chronyd -path /usr/local/bin/restart-chronyd -mode 0755 \
 	unit -source chronyd-restart.service -enable=false \
-    unit -source chronyd-restart.timer \
+	unit -source chronyd-restart.timer \
 	dropin -source 20-conditional-start.conf -for chronyd.service	
-
