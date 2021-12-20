d8e186431e45192d2be6e7422d7a8f1d5177cbad
Sabina Aledort <saledort@redhat.com>
2021-12-20 14:43:03 +0200

    infra: TEMP: use metallb frr image from ci registry
    The quay.io/openshift/origin-metallb-frr image should
    be made public. Till that is done we can deploy the
    image from the CI registry.

--- hack/setup-build-index-image.sh
+++ hack/setup-build-index-image.sh
@@ -63,6 +63,8 @@ spec:
           # "release-4.10" should be changed to "OPERATOR_RELEASES" when moving all operators to 4.10.
           git clone --single-branch --branch release-4.10 https://github.com/openshift/metallb-operator.git
           cd metallb-operator
+          # TODO: temp sed till "quay.io/openshift/origin-metallb-frr" is public.
+          sed -i "s#quay.io/openshift/origin-metallb-frr:4.10#registry.ci.openshift.org/origin/4.10:metallb-frr#" manifests/4.10/metallb-operator.v4.10.0.clusterserviceversion.yaml
           podman build -f bundleci.Dockerfile --tag image-registry.openshift-image-registry.svc:5000/openshift-marketplace/metallb-operator-bundle:latest .
           podman push image-registry.openshift-image-registry.svc:5000/openshift-marketplace/metallb-operator-bundle:latest --tls-verify=false
           cd ..
