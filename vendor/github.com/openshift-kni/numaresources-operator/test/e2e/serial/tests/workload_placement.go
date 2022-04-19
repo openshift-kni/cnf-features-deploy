50e78eb13d251f3a3828efa7d7dde36e7f937340
Francesco Romani <fromani@redhat.com>
2022-04-19 13:18:36 +0200

    cnf-tests: bump numaresources to v0.4.10-3.2022042201
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- vendor/github.com/openshift-kni/numaresources-operator/test/e2e/serial/tests/workload_placement.go
+++ vendor/github.com/openshift-kni/numaresources-operator/test/e2e/serial/tests/workload_placement.go
@@ -1,26 +1,24 @@
 /*
-Copyright 2022 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright 2022 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package tests
 
 import (
 	"context"
 	"fmt"
-	"os"
-	"strconv"
 	"time"
 
 	"github.com/ghodss/yaml"
@@ -608,23 +606,4 @@ func matchLogLevelToKlog(cnt *corev1.Container, level operatorv1.LogLevel) (bool
 
 	val, found := rteFlags.GetFlag("--v")
 	return found, val.Data == kLvl.String()
-}
-
-func skipUnlessEnvVar(envVar, message string) {
-	if isEnvTrue(envVar) {
-		return
-	}
-	Skip(message, 1)
-}
-
-func isEnvTrue(envVar string) bool {
-	val, ok := os.LookupEnv(envVar)
-	if !ok {
-		return false
-	}
-	enabled, err := strconv.ParseBool(val)
-	if err != nil {
-		return false
-	}
-	return enabled
 }
