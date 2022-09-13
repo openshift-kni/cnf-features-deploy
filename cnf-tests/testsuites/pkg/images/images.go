87b7c4ad625a12a5faee3f77913dbe2c45d08d62
Sabina Aledort <saledort@redhat.com>
2022-09-13 12:17:49 +0300

    infra: Bump CNF deployment and tests to 4.12
    We should now run 4.12 versions on master.

--- cnf-tests/testsuites/pkg/images/images.go
+++ cnf-tests/testsuites/pkg/images/images.go
@@ -29,12 +29,12 @@ func init() {
 
 	cnfTestsImage = os.Getenv("CNF_TESTS_IMAGE")
 	if cnfTestsImage == "" {
-		cnfTestsImage = "cnf-tests:4.11"
+		cnfTestsImage = "cnf-tests:4.12"
 	}
 
 	dpdkTestImage = os.Getenv("DPDK_TESTS_IMAGE")
 	if dpdkTestImage == "" {
-		dpdkTestImage = "dpdk:4.11"
+		dpdkTestImage = "dpdk:4.12"
 	}
 
 	images = map[string]imageLocation{
