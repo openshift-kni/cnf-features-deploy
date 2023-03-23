d2c67f645b4e3b23417d2c2dd534e8f3d8eee70d
Andrea Panattoni <apanatto@redhat.com>
2023-03-23 19:06:43 +0100

    cnf-tests: Make junit report readable by Prow
    CI Operator needs junit output files to be in the
    form "junit*.xml" to be showed in the job page.
    
    Signed-off-by: Andrea Panattoni <apanatto@redhat.com>

--- cnf-tests/hack/fill-empty-docs.sh
+++ cnf-tests/hack/fill-empty-docs.sh
@@ -11,5 +11,5 @@ entrypoint/test-run.sh -junit _cache/junit --ginkgo.dry-run -ginkgo.v
 
 go build -o _cache/docgen docgen/main.go 
 # use the junit files to fill the descriptions
-_cache/docgen fill --junit _cache/junit/cnftests-junit.xml --description docgen/e2e.json
-_cache/docgen fill --junit _cache/junit/validation_junit.xml --description docgen/validation.json
+_cache/docgen fill --junit _cache/junit/junit_cnftests.xml --description docgen/e2e.json
+_cache/docgen fill --junit _cache/junit/junit_validation.xml --description docgen/validation.json
