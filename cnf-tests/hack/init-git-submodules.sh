a1149c5802c4061d6d7c70bd8fa92dff8dbe8070
liornoy <lnoy@redhat.com>
2023-05-09 19:49:30 +0300

    cnf-tests: Add git-submodules
    - Add the git-submodules: sriov, metallb, nto.
    - Add cnf-tests/hack/init-git-submodules script that
    fetches the git-submodules with the required branch.
    - Add Make target to call the init-git-submodules script.
    - Exclude the submodules dir from the gofmt and lint script,
    that in order to satisfy the CI.
    
    Signed-off-by: liornoy <lnoy@redhat.com>

---
+++ cnf-tests/hack/init-git-submodules.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"/..
+
+set -x
+set -e
+
+if [ -n "$TARGET_RELEASE" ]; then
+    METALLB_OPERATOR_TARGET_COMMIT="$TARGET_RELEASE"
+    SRIOV_NETWORK_OPERATOR_TARGET_COMMIT="$TARGET_RELEASE"
+    CLUSTER_NODE_TUNING_OPERATOR_TARGET_COMMIT="$TARGET_RELEASE"
+fi
+
+echo "metallb-operator target commit: ${METALLB_OPERATOR_TARGET_COMMIT}"
+echo "sriov-operator target commit: ${SRIOV_NETWORK_OPERATOR_TARGET_COMMIT}"
+echo "cluster-node-tuning-operator target commit: ${CLUSTER_NODE_TUNING_OPERATOR_TARGET_COMMIT}"
+
+cd submodules/metallb-operator/
+git fetch --all
+git checkout origin/"${METALLB_OPERATOR_TARGET_COMMIT}"
+
+cd ../sriov-network-operator/
+git fetch --all
+git checkout origin/"${SRIOV_NETWORK_OPERATOR_TARGET_COMMIT}"
+
+cd ../cluster-node-tuning-operator/
+git fetch --all
+git checkout origin/"${CLUSTER_NODE_TUNING_OPERATOR_TARGET_COMMIT}"
