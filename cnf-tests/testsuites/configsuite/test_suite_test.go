e7335ba22da0ab3a38ecfcecaab0cf4357371354
Lior Noy <lnoy@redhat.com>
2023-08-17 17:52:15 +0300

    cnf-tests: Move the k8sreporter to the FailHandler
    This commit moves the reporter.Dump() call into the FailHandler.
    We do this because previously, having the reporter dump in the
    ReportAfterEach, we didn't get the desired logs because
    the test namesapce would be cleaned beforehand in the AfterEach/
    AfterAll phase.
    
    Signed-off-by: Lior Noy <lnoy@redhat.com>

--- cnf-tests/testsuites/configsuite/test_suite_test.go
+++ cnf-tests/testsuites/configsuite/test_suite_test.go
@@ -38,7 +38,13 @@ func init() {
 }
 
 func TestTest(t *testing.T) {
-	RegisterFailHandler(Fail)
+	RegisterFailHandler(
+		func(message string, callerSkip ...int) {
+			if reporter != nil {
+				reporter.Dump(testutils.LogsExtractDuration, CurrentSpecReport().LeafNodeText)
+			}
+			Fail(message, callerSkip...)
+		})
 
 	if *reportPath != "" {
 		reportFile := path.Join(*reportPath, "setup_failure_report.log")
@@ -71,15 +77,5 @@ var _ = ReportAfterSuite("setup", func(report types.Report) {
 
 	if qe_reporters.Polarion.Run {
 		reporters.ReportViaDeprecatedReporter(&qe_reporters.Polarion, report)
-	}
-})
-
-var _ = ReportAfterEach(func(specReport types.SpecReport) {
-	if specReport.Failed() == false {
-		return
-	}
-
-	if *reportPath != "" {
-		reporter.Dump(testutils.LogsExtractDuration, specReport.LeafNodeText)
 	}
 })
--- cnf-tests/testsuites/e2esuite/test_suite_test.go
+++ cnf-tests/testsuites/e2esuite/test_suite_test.go
@@ -80,7 +80,13 @@ func init() {
 }
 
 func TestTest(t *testing.T) {
-	RegisterFailHandler(Fail)
+	RegisterFailHandler(
+		func(message string, callerSkip ...int) {
+			if reporter != nil {
+				reporter.Dump(testutils.LogsExtractDuration, CurrentSpecReport().LeafNodeText)
+			}
+			Fail(message, callerSkip...)
+		})
 
 	if *reportPath != "" {
 		reportFile := path.Join(*reportPath, "cnftests_failure_report.log")
@@ -121,15 +127,5 @@ var _ = ReportAfterSuite("cnftests", func(report types.Report) {
 	}
 	if qe_reporters.Polarion.Run {
 		reporters.ReportViaDeprecatedReporter(&qe_reporters.Polarion, report)
-	}
-})
-
-var _ = ReportAfterEach(func(specReport types.SpecReport) {
-	if specReport.Failed() == false {
-		return
-	}
-
-	if *reportPath != "" {
-		reporter.Dump(testutils.LogsExtractDuration, specReport.LeafNodeText)
 	}
 })
