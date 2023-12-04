2cfd0a05d46bd1e608e6be1e37285e3d65fb37ff
shajmakh <shajmakh@redhat.com>
2023-12-04 15:36:20 +0200

    vendor: bump NTO update
    Update NTO latest commit in go.mod to consume latest updates:
    update go.mod with latest NTO PR's hash.
    run `make deps-update`.
    
    Signed-off-by: shajmakh <shajmakh@redhat.com>

--- vendor/github.com/openshift/cluster-node-tuning-operator/pkg/performanceprofile/controller/performanceprofile/components/profile/profile.go
+++ vendor/github.com/openshift/cluster-node-tuning-operator/pkg/performanceprofile/controller/performanceprofile/components/profile/profile.go
@@ -9,7 +9,7 @@ import (
 
 // GetMachineConfigPoolSelector returns the MachineConfigPoolSelector from the CR or a default value calculated based on NodeSelector
 func GetMachineConfigPoolSelector(profile *performancev2.PerformanceProfile, profileMCP *mcov1.MachineConfigPool) map[string]string {
-	// we do not really need profile.spec.machineConfigPoolSelector anymore, but we should use it for backward compatability
+	// we do not really need profile.spec.machineConfigPoolSelector anymore, but we should use it for backward compatibility
 	if profile.Spec.MachineConfigPoolSelector != nil {
 		return profile.Spec.MachineConfigPoolSelector
 	}
@@ -50,6 +50,21 @@ func IsPaused(profile *performancev2.PerformanceProfile) bool {
 
 	isPaused, ok := profile.Annotations[performancev2.PerformanceProfilePauseAnnotation]
 	if ok && isPaused == "true" {
+		return true
+	}
+
+	return false
+}
+
+// IsCgroupsVersionIgnored returns whether or not the performance profile's cgroup
+// downgrade logic should be executed
+func IsCgroupsVersionIgnored(profile *performancev2.PerformanceProfile) bool {
+	if profile.Annotations == nil {
+		return false
+	}
+
+	isIgnored, ok := profile.Annotations[performancev2.PerformanceProfileIgnoreCgroupsVersion]
+	if ok && isIgnored == "true" {
 		return true
 	}
 
