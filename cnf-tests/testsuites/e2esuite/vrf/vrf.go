8e65745d36db54cca901f3fb084ceb4c7b540aa9
Andrea Panattoni <apanatto@redhat.com>
2023-12-05 17:01:05 +0100

    cnf-tests: Add context to VRF test failures
    Signed-off-by: Andrea Panattoni <apanatto@redhat.com>

--- cnf-tests/testsuites/e2esuite/vrf/vrf.go
+++ cnf-tests/testsuites/e2esuite/vrf/vrf.go
@@ -1,6 +1,7 @@
 package vrf
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -84,17 +85,23 @@ var _ = Describe("[vrf]", func() {
 	})
 })
 
-func pingIPViaVRF(cs *client.ClientSet, client *k8sv1.Pod, vrfName string, DestIPAddr string) error {
+func pingIPViaVRF(cs *client.ClientSet, client *k8sv1.Pod, vrfName string, DestIPAddr string) (bytes.Buffer, error) {
 	pingCommand := []string{"ping", "-I", vrfName, "-c5", DestIPAddr}
-	pod, err := cs.Pods(client.Namespace).Get(context.Background(), client.Name, metav1.GetOptions{})
-	pingStatus, err := pods.ExecCommand(cs, *pod, pingCommand)
-	if err != nil {
-		return err
-	}
-	if strings.Contains(pingStatus.String(), " 0% packet loss") {
-		return nil
-	}
-	return fmt.Errorf("Connectivity test error")
+	return pods.ExecCommand(cs, *client, pingCommand)
+}
+
+func assertIPIsReachableViaVRF(cs *client.ClientSet, client *k8sv1.Pod, vrfName string, DestIPAddr string) {
+	output, err := pingIPViaVRF(cs, client, vrfName, DestIPAddr)
+
+	ExpectWithOffset(1, err).ToNot(HaveOccurred())
+	ExpectWithOffset(1, output.String()).To(ContainSubstring(" 0% packet loss"))
+}
+
+func assertIPIsNotReachableViaVRF(cs *client.ClientSet, client *k8sv1.Pod, vrfName string, DestIPAddr string) {
+	output, err := pingIPViaVRF(cs, client, vrfName, DestIPAddr)
+
+	ExpectWithOffset(1, err).To(HaveOccurred(), "Expected ping error, but output was: %s", output)
+	ExpectWithOffset(1, err.Error()).To(ContainSubstring(" 100% packet loss"))
 }
 
 func addVRFNad(cs *client.ClientSet, NadName string, vrfName string) netattdefv1.NetworkAttachmentDefinition {
@@ -221,13 +228,12 @@ func testVRFScenario(apiclient *client.ClientSet, namespace string, nodeName str
 		[]map[string]string{
 			{"vrfName": VRFBlueName, "vrfClientIP": podServerFirstVRFNetworkIPAddress, "vrfInterface": "net1"},
 			{"vrfName": VRFRedName, "vrfClientIP": podServerSecondVRFNetworkOverlappingIP, "vrfInterface": "net2"}})
-	err := pingIPViaVRF(apiclient, podClient, VRFRedName, podServerSecondVRFNetworkOverlappingIP)
-	Expect(err).ToNot(HaveOccurred())
+	assertIPIsReachableViaVRF(apiclient, podClient, VRFRedName, podServerSecondVRFNetworkOverlappingIP)
 
 	By("Run client/server ICMP connectivity tests")
-	err = pingIPViaVRF(apiclient, podClient, VRFBlueName, podServerFirstVRFNetworkIPAddress)
-	Expect(err).ToNot(HaveOccurred())
-	err = apiclient.Pods(namespace).Delete(context.Background(), podServer.Name, metav1.DeleteOptions{GracePeriodSeconds: pointer.Int64Ptr(0)})
+	assertIPIsReachableViaVRF(apiclient, podClient, VRFBlueName, podServerFirstVRFNetworkIPAddress)
+
+	err := apiclient.Pods(namespace).Delete(context.Background(), podServer.Name, metav1.DeleteOptions{GracePeriodSeconds: pointer.Int64Ptr(0)})
 	Expect(err).ToNot(HaveOccurred())
 	Eventually(func() error {
 		_, err := apiclient.Pods(namespace).Get(context.Background(), podServer.Name, metav1.GetOptions{})
@@ -235,10 +241,7 @@ func testVRFScenario(apiclient *client.ClientSet, namespace string, nodeName str
 	}, podWaitingTime, 5*time.Second).Should(HaveOccurred())
 
 	By("Run client/server ICMP negative connectivity tests")
-	err = pingIPViaVRF(apiclient, podClient, VRFBlueName, podServerFirstVRFNetworkIPAddress)
-	Expect(err).To(HaveOccurred())
-	err = pingIPViaVRF(apiclient, podClient, VRFRedName, podServerSecondVRFNetworkOverlappingIP)
-	Expect(err).To(HaveOccurred())
-	err = pingIPViaVRF(apiclient, podClient, "eth0", podServerSecondVRFNetworkOverlappingIP)
-	Expect(err).ToNot(HaveOccurred())
+	assertIPIsNotReachableViaVRF(apiclient, podClient, VRFBlueName, podServerFirstVRFNetworkIPAddress)
+	assertIPIsNotReachableViaVRF(apiclient, podClient, VRFRedName, podServerSecondVRFNetworkOverlappingIP)
+	assertIPIsReachableViaVRF(apiclient, podClient, "eth0", podServerSecondVRFNetworkOverlappingIP)
 }
