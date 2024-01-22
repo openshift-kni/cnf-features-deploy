9d6c754e744f36eec98dc7f1ea9ecfc8500b1c72
nkononov <nkononov@redhat.com>
2024-01-22 11:00:10 -0500

    cnf-tests: fec fixed import order

--- cnf-tests/testsuites/e2esuite/fec/fec.go
+++ cnf-tests/testsuites/e2esuite/fec/fec.go
@@ -3,12 +3,13 @@ package fec
 import (
 	"context"
 	"fmt"
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	corev1 "k8s.io/api/core/v1"
 	"strings"
 	"time"
 
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
