508842e21b2ec5dd7b9cd8fbc8608862a9e5dd52
Steven Skeard <sskeard@redhat.com>
2024-10-09 15:39:19 -0400

    ztp: CNF-14638: Align on go 1.21 - We are already using golang-1.21 builders - Several old fields were still set to go 1.20 - The policy gen plugin is currently using go 1.22 on latest but that would require an ocp 4.17 builder so checkout the last commit before they updated it

--- ztp/tools/pgt2acmpg/scripts/build-acmpg-plugin.sh
+++ ztp/tools/pgt2acmpg/scripts/build-acmpg-plugin.sh
@@ -10,8 +10,10 @@ CNF_FEATURE_DEPLOY_ROOT=$SCRIPT_DIR/../../../..
 TMP_DIR=$(mktemp -d -p .)
 echo "Created $TMP_DIR"
 cd "$TMP_DIR"
-git clone --depth 1 --branch main --single-branch https://github.com/open-cluster-management-io/policy-generator-plugin.git
+git clone --branch main --single-branch https://github.com/open-cluster-management-io/policy-generator-plugin.git
 cd policy-generator-plugin
+# This is the last commit where the policy generator was on go 1.21
+git checkout eb5c12308072d4bc5a36e197e3cbb84cc23e7a82
 go mod vendor
 make build-binary
 cp PolicyGenerator "$1"
