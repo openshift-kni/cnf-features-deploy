d5500d4e46bcb798e24e437657296c782438f95b
Andrea Panattoni <apanatto@redhat.com>
2025-01-20 14:38:57 +0100

    cnf-tests: Bump `OPERATOR_VERSION` to 4.19
    Since the 4.18 branch has been cut, master branch
    should deploy 4.19 operators
    
    Signed-off-by: Andrea Panattoni <apanatto@redhat.com>

--- cnf-tests/testsuites/pkg/images/images.go
+++ cnf-tests/testsuites/pkg/images/images.go
@@ -29,12 +29,12 @@ func init() {
 
 	cnfTestsImage = os.Getenv("CNF_TESTS_IMAGE")
 	if cnfTestsImage == "" {
-		cnfTestsImage = "cnf-tests:4.18"
+		cnfTestsImage = "cnf-tests:4.19"
 	}
 
 	dpdkTestImage = os.Getenv("DPDK_TESTS_IMAGE")
 	if dpdkTestImage == "" {
-		dpdkTestImage = "dpdk:4.16"
+		dpdkTestImage = "dpdk:4.19"
 	}
 
 	images = map[string]imageLocation{
